02f49811b32d162489c7bc06977d3474ee003bb0
Giap <togiap.dev@gmail.com>
2024-02-22 16:25:48 +0700

    update

---
+++ heardlove/layouts/passform.tsx
@@ -0,0 +1,95 @@
+'use client'
+import React, { useEffect, useState } from "react";
+import "@/stylesheet/passform.css";
+interface PassFormProps {
+    onClose: () => void;
+}
+
+const PassForm: React.FC<PassFormProps> = ({onClose}) => {
+    const fake_pass_word = "iuanhbap";
+    const pass_word = "10102009";
+    const [err1, setErr1] = useState(false);
+    const [err2, setErr2] = useState(false);
+    const [err3, setErr3] = useState(false);
+    const [err4, setErr4] = useState(false);
+    const [valuePass, setValuePass] = useState("");
+    const [checkBoxAccept, setCheckBoxAccept] = useState(false);
+    const [checkBoxReject, setCheckBoxReject] = useState(false);
+    const [disableAccept, setDisableAccept] = useState(false);
+    const [disableReject, setDisableReject] = useState(false);
+    const [unLock, setUnLock] = useState(false);
+    const followChangePass = (e: any) => {
+        const value = e.target.value;
+        setValuePass(value)
+    };
+    const followCheckAccept = (e: any) => {
+        setCheckBoxAccept(e.target.checked);
+    };
+    const followCheckReject = (e:any) => {
+        setCheckBoxReject(e.target.checked);
+    };
+    useEffect(() => {
+        checkBoxAccept ? setDisableReject(true) : setDisableReject(false);
+        checkBoxReject ? setDisableAccept(true) : setDisableAccept(false);
+    }, [checkBoxAccept, checkBoxReject]);
+    useEffect(() => {
+        checkBoxReject ? setErr2(true) : setErr2(false);
+    },[checkBoxReject]);
+    useEffect(() => {
+        if( checkBoxAccept || checkBoxReject) {setErr3(false);}
+    }, [checkBoxAccept, checkBoxReject]);
+    useEffect(() => {
+        if(valuePass === pass_word && checkBoxAccept) {
+            setUnLock(true);
+        } else{
+            setUnLock(false);
+        }
+    }, [valuePass, checkBoxAccept])
+    const handleSubmit = () => {
+        valuePass === "" && setErr1(true);
+        valuePass !== "" && setErr1(false);
+        !checkBoxAccept && !checkBoxReject && setErr3(true);
+        if( checkBoxAccept || checkBoxReject) {setErr3(false);}
+        if (valuePass === "iuanhbap") {
+            alert("Hí Hí! Trôn Trôn Việt Nam. PassWord là ngày tháng năm sinh của em bé đóa :3");
+        }
+        if (valuePass !== pass_word) {
+            setErr4(true);
+        }
+    };
+    return (
+        <div className="pass-form">
+            <button onClick={onClose} className="close-btn">close</button>
+            <div className="form-password">
+                <label htmlFor="password">&#42; Để mở được hộp quà, em bé nhập pass là : <span className="pass-display">{fake_pass_word}</span></label>
+                <input onChange={followChangePass} type="text" name="password" id="password" />
+                <span className="err-pass">{err1 ? "* Không để trống mục này" : ""}</span>
+                <span className="err-pass">{err4 ? "* Sai mật khẩu" : ""}</span>
+                <div className="terms">
+                    <div className="terms-item">
+                        <input disabled={disableAccept} onChange={followCheckAccept} type="checkbox" name="" id="accept" />
+                        <label className="checkbox" htmlFor="accept"></label>
+                        <label htmlFor="accept">Đồng ý cưới abe :3</label>
+                    </div>
+                    <div className="terms-item">
+                        <input disabled={disableReject} onChange={followCheckReject} type="checkbox" name="" id="reject" />
+                        <label className="checkbox" htmlFor="reject"></label>
+                        <label htmlFor="reject">Cưới anh Chí Công &#9785;</label>                    
+                    </div>
+                    <div className="err2">{err2 ? "Chạ iu anh :((" : ""}</div>
+                    <div className="err3">{err3 ? "Chọn một trong hai đi :))" : ""}</div>
+                </div>
+                <button onClick={handleSubmit} className="btn-accept">
+                    {unLock ? 
+                        (
+                            <a href="./">Đồng ý</a>
+                        ) : 
+                        "Đồng ý"
+                   }
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default PassForm;
--- heardlove/layouts/venus.tsx
+++ heardlove/layouts/venus.tsx
@@ -1,9 +1,17 @@
 'use client'
-import React from "react"
+import React, {useState} from "react"
 import "@/stylesheet/venus.css";
 import TextType from "@/functions/texttype";
-const Venus = () => {
+import PassForm from "./passform";
+const Venus: React.FC = () => {
     const text_lesson = "Hí em bé! Em bé cũng đã đến hành tinh cuối cùng gòi. Ở đây thì không có gì nhiều đâu nha, anh chỉ muốn dành cho em bé vài lời thôi nè. Trong thời gian qua, yêu em bé có những lúc vui vẻ, nhưng cũng có những lúc giận hờn lẫn nhau. Tuy nhiên cả 2 vẫn tiếp tục đi tiếp với nhau chứ không dừng lại. Đây thực sự là một điều cực kì tốt luôn. Anh bé cảm ơn Bống vì đã ở lại với anh, chấp nhận anh. Mặc dù anh còn nhiều khuyết điểm, là một người con trai tệ nhưng em vẫn luôn đồng hành cùng anh trong thời gian qua. Trong thời gian hơn 3 tháng yêu nhau, không ngắn cũng không dài, anh đã thực sự chắc chắn rằng muống cưới em bé, muốn cùng em bé đi đến hôn nhân và có một gia đình thật hạnh phúc. Anh sẽ cố gắng để hoàn thiện bản thân hơn, sẽ thay đổi để trở thành một con người tốt nhất để có thể qua nhà em bé chào hỏi bố mẹ nè. Tới đây thì anh cũng có chút quà nho nhỏ cho em bé nữa, nhớ mở quà nhoa :3."
+    const [isOpen, setIsOpen] = useState(false);
+    const closePassForm = () => {
+        setIsOpen(false);
+    }
+    const openPassForm = () => {
+        setIsOpen(true);
+    }
     return (
         <div className="venus-planet">
             <div className="head-planet">
@@ -17,10 +25,15 @@ const Venus = () => {
             <div className="text-planet">
                 <TextType text={text_lesson}/>
             </div>
-            <button className="btn-gift">
+            <button onClick={openPassForm} className="btn-gift">
                 <img src="./gift.png" alt="" />
                 <div className="text">Mở quà</div>
             </button>
+            {isOpen && (
+                <div className="passform">
+                    <PassForm onClose={closePassForm}/>
+                </div>
+            )}
         </div>
     )
 }
